f5cafc08a6173a8682cefb1c3cf27e23ac425484
KiberPerdun <Suisuikin@yandex.ru>
2025-12-15 22:39:59 +0300

    fix paths

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: FC = () => {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/landing" element={<LandingPage />} />
-          <Route path="/layers/:id" element={<LayerDetailPage />} />
+          <Route path="/layer/:id" element={<LayerDetailPage />} />
           <Route path="/chrono" element={<HomePage />} />
           <Route path="/services" element={<ServiceListPage />} />
           <Route
